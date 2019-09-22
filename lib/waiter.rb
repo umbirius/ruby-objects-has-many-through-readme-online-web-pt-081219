94e6948c355a2a2ef0011c47aa38a9cd09943ad9
Umberto Palazzo <upalazzo93@gmail.com>
2019-09-22 23:41:54 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -16,6 +16,15 @@ class Waiter
   end 
   
   def new_meal(customer,total,tip)
-    Meal.new()
+    Meal.new(self,customer,total,tip)
+  end 
+  
+  def meals
+    Meals.all.select {|meal| meal.waiter == self}
+  end 
+  
+  def best_tipper 
+    meals.find {|tip| meal.tip}
+  end 
 
 end
