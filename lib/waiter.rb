187bcb906d85c460216557b4ab83e7cc502331a3
Umberto Palazzo <upalazzo93@gmail.com>
2019-09-22 23:20:03 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,11 +1,18 @@
 class Waiter
   
   attr_accessor :years 
-  attr_reader :name 
+  attr_reader :name
+  
+  @@all = []
 
   def initialize(name, years)
     @name = name 
     @years = years 
+    @@all << self 
   end
 
+  def self.all
+    @@all 
+  end 
+  
 end
